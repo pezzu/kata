bf92c3ea15e71c1e6c73d2b4505f56a95f17e938
pezzu <7243796+pezzu@users.noreply.github.com>
2020-07-31 23:44:37 -0400

    Now we can get initial source code

--- kata.js
+++ kata.js
@@ -1,5 +1,6 @@
 const bent = require('bent')
 const fs = require('fs')
+const Nightmare = require('Nightmare')
 
 const { extractId } = require('./cw-url-parser')
 
@@ -14,20 +15,38 @@ if (!kataId) {
   const kata = await getKata(kataId)
   const code = {
     name: kata.slug + '.js',
-    data: getCode(kata)
+    data: genSource(kata)
   }
 
   const spec = {
     name: kata.slug + '.spec.js',
-    data: getTests(kata)
+    data: genTests(kata)
   }
 
   saveKata(kata, [code, spec]);
 })()
 
 async function getKata(slug) {
+  return Promise.all([getKataInfo(slug), getKataCode(slug)])
+    .then(([ kata, code ]) => ({ ...kata, code }));
+}
+
+async function getKataInfo(slug) {
   const get = bent('https://www.codewars.com/', 'GET', 'json')
   return get('api/v1/code-challenges/' + slug)
+}
+
+async function getKataCode(slug) {
+  const nightmare = Nightmare()
+  return nightmare
+    .goto(`https://www.codewars.com/kata/${slug}/train/javascript`)
+    .wait('#code_container')
+    .evaluate(() => Array.from(
+       document.getElementsByClassName('CodeMirror-lines')[0]
+      .getElementsByClassName('CodeMirror-line'))
+      .map(e => e.outerText)
+      .join('\n'))
+    .end()
 }
 
 function saveKata(kata, files) {
@@ -41,10 +60,10 @@ function getMarkDown(kata) {
   return `/**\n# [${kata.name}](${kata.url}) (${kata.rank.name})\n---\n${kata.description}\n*/`
 }
 
-function getCode(kata) {
-  return getMarkDown(kata)
+function genSource(kata) {
+  return `${getMarkDown(kata)}\n\n${kata.code}`
 }
 
-function getTests(kata) {
+function genTests(kata) {
   return `require('./${kata.slug}.js');`
 }
